a9d9466b76e3bb06016443dd977ec3debac52ac1
eduardovale76 <eduardovale76@gmail.com>
2022-08-24 15:41:53 -0300

    Criar env-sample

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)+sy482rl@7#aoo$f*ta1)a@i)7noo=c4vsi7!=e_#x&9_-+u8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
