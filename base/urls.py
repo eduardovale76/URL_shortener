4369cab97954f6dea42eca3932de8b262f0fd825
eduardovale76 <eduardovale76@gmail.com>
2022-09-19 16:29:22 -0300

    Shortner template and copy button

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,7 @@
+from html.entities import name2codepoint
+
 from django.urls import path
 
-from .views import index
+from .views import index, shortner
 
-urlpatterns = [
-    path("", index, name="index"),
-]
+urlpatterns = [path("", index, name="index"), path("shortner/", shortner, name="shortner")]
